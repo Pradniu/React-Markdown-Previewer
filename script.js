7a2791d50875ac33f0d9b28e1a438924b5b95f41
Pradeep Niure <64963390+Pradniu@users.noreply.github.com>
2026-07-13 19:18:11 +1000

    Uploaded chrome html, javascript and css

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;}const initialState = `
+const initialState=`
 # Welcome>/. To My Text Previewer !
  # -------------------------------------
 
@@ -32,38 +32,38 @@ y=1+z
 ![React](data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K)
 `;
 
-class App extends React.Component {constructor(...args) {super(...args);_defineProperty(this, "state",
-    {
-      text: initialState });_defineProperty(this, "handleChange",
-
-    e => {
-      this.setState({
-        text: e.target.value });
-
-    });}
-
-  render() {
-    const { text } = this.state;
-    const markdown = marked(text, { breaks: true });
-    return /*#__PURE__*/(
-      React.createElement("div", { class: "container" }, /*#__PURE__*/
-      React.createElement("h1", null), /*#__PURE__*/
-      React.createElement("div", { className: "row justify-content-md-center" }, /*#__PURE__*/
-      React.createElement("div", { className: "col-md-6 first" }, /*#__PURE__*/
-      React.createElement("h6", null, /*#__PURE__*/React.createElement("i", { class: "fab fa-free-code-camp" }), " \xA0Editor"), /*#__PURE__*/
-
-      React.createElement("textarea", { className: "form-control p-4", id: "editor", value: text, onChange: this.handleChange })), /*#__PURE__*/
-
-
-      React.createElement("div", { className: "col-md-8  second", id: "preview" }, /*#__PURE__*/
-      React.createElement("h6", null, /*#__PURE__*/React.createElement("i", { class: "fab fa-free-code-camp" }), " \xA0 Previewer"), /*#__PURE__*/
-
-      React.createElement("div", { className: "preview p-4", dangerouslySetInnerHTML: { __html: markdown } })))));
-
-
-
-
-
-  }}
-
-ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('app'));
+class App extends React.Component {
+  state={
+    text:initialState
+  };
+  handleChange=(e)=>{
+    this.setState({
+      text:e.target.value
+    })
+  }
+  
+  render () {
+    const {text}=this.state;
+    const markdown=marked(text, { breaks:true });
+    return( 
+      <div class="container">
+        <h1></h1>
+ <div className="row justify-content-md-center">  
+        <div className="col-md-6 first">
+                  <h6><i class="fab fa-free-code-camp"></i>	&nbsp;Editor</h6>
+  
+          <textarea className="form-control p-4" id="editor" value={text} onChange={this.handleChange}/>
+         
+        </div>
+        <div className="col-md-8  second"  id="preview">
+            <h6><i class="fab fa-free-code-camp"></i>	&nbsp; Previewer</h6>
+          
+          <div className="preview p-4" dangerouslySetInnerHTML={{__html:markdown}}/>
+          
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+ReactDOM.render(<App/>,document.getElementById('app'));
